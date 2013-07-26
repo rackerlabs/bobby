f685a5c5c85a79462f377ff75698123c4f2620ec
Ken Wronkiewicz <ken.wronkiewicz@cloudkick.com>
2013-07-26 11:30:56 -0700

    adding worker API in sketch form.

---
+++ bobby/worker.py
@@ -0,0 +1,19 @@
+"""
+Functions for actually doing things
+"""
+
+def create_server_entity():
+	""" Creates a server's entity in MaaS """
+	pass
+
+def apply_policies_to_server():
+	""" Apply policies to a new server """
+	pass
+
+def remove_server():
+	""" Clean up a server's records """
+	pass
+
+def apply_policy():
+	"""Apply a new policy accross a group of servers"""
+	pass
